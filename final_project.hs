e43a28cd50042ada80f4f6536418c067e167a6d6
melissagrueter <melissa.grueter@gmail.com>
2016-04-16 02:21:21 -0700

    Made it funnier! ;Can almost generate random stuff

--- final_project.hs
+++ final_project.hs
@@ -1,19 +1,88 @@
+--Melissa Grueter
+--Gloria Liou
+
 import Data.Word
 import Data.IORef
+import System.Random
+
+data Professor = Bruce | Chen | Greenberg | Kauchak | Wu deriving Show
+data Class = CS52 | CS62 | CS81 | Systems | Algs deriving Show
+data Location = Beanbag | Hall | Office | Lab | Edmunds101 deriving Show
+
+randomNumbers :: Int -> [Int]
+randomNumbers seed = take 1 . randomRs (1, 5) . mkStdGen $ seed
+
+--data World = (Professor * Class * Location) 
+--genCorrectWorld
+
+--genCorrectWorld = do
+
+--prof = randomNumbers 47
+--weapon = randomNumbers 181
+--location = randomNumbers 52
+
+genCorrectProf = case (randomNumbers 4) of
+                           [1] -> Bruce
+                           [2] -> Chen
+                           [3] -> Greenberg
+                           [4] -> Kauchak
+                           [5] -> Wu
+                           
+genCorrectClass = case (randomNumbers 5) of
+                           [1] -> CS52
+                           [2] -> CS62
+                           [3] -> CS81
+                           [4] -> Systems
+                           [5] -> Algs
+                           
+genCorrectLocation = case (randomNumbers 3) of
+                           [1] -> Beanbag
+                           [2] -> Hall
+                           [3] -> Office
+                           [4] -> Lab
+                           [5] -> Edmunds101
+
+
+--data World = World {prof :: Professor, weapon :: Class, location :: Location}
+
+prof = genCorrectProf
+weapon = genCorrectClass
+location = genCorrectLocation
+
+--genCorrectWorld = World {prof, weapon, location}
+     
+
+--from random number pick prof/class/room
+
+--genPossibleWorlds
 
 prompt :: IO ()
-prompt = do 
- putStrLn ("Who killed the student?\nEnter 'bruce', 'chen', 'greenberg', 'kauchak', or 'wu':")
- s <- getLine 
- putStrLn ("You said "++s++" killed the student.")
+prompt = do
 
- putStrLn "Which class did (s)he use?\nEnter '52', '62', '81', 'systems', or 'algs':"
- s <- getLine 
- putStrLn ("You said (s)he used "++s++".")
+--thin possible worlds
 
- putStrLn "Where did (s)he kill the student?\nEnter 'beanbag', 'hall', 'office', 'lab', or 'edmunds101':"
+ putStrLn ("Who killed the student?\nEnter 'Bruce', 'Chen', 'Greenberg', 'Kauchak', or 'Wu':")
+ s <- getLine
+ if s /= "bruce"
+    then
+        do putStrLn (""++s++" is innocent! How dare you suspect them!")
+ else putStrLn ("Correct! "++s++" was the cold-blooded killer.")
+
+ putStrLn "Which class did (s)he use?\nEnter 'CS52', 'CS62', 'CS81', 'Systems', or 'Algs':"
+ s <- getLine
+ if s /= "systems"
+    then
+        do putStrLn ("Please, "++s++" is easy! That class never killed anybody.")
+ else putStrLn ("Correct! "++s++" was the murder weapon... cruel and unusual torture indeed!")
+
+ putStrLn "Where did (s)he kill the student?\nEnter 'Beanbag', 'Hall', 'Office', 'Lab', or 'Edmunds101':"
  s <- getLine 
- putStrLn ("You said (s)he killed the student in "++s++".")
+ if s /= "beanbag"
+    then
+        do putStrLn ("The "++s++"? Nothing ever happens there.")
+           putStrLn ("Let's try that again. Hurry up, before another CS student gets killed.")
+           prompt 
+ else putStrLn ("Correct! The "++s++"... the perfect place to kill someone!")
 
 --how do we print this one line at a time?
 --make this funnier LOL
@@ -49,5 +118,8 @@ main = do
  putStrLn ("Lori's office?")
  putStrLn ("The downstairs lab?")
  putStrLn ("Edmunds 101?\n")
- 
+
+ --genCorrectWorld
+ --genPossibleWords
  prompt
+
